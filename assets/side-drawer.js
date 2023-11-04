632d75ee818b17bf284f89e7c738e0f2d9a0bde4
johnlewisdt <74916430+johnlewisdt@users.noreply.github.com>
2023-11-04 15:52:09 +0000

    Adds drawer toggle functions

--- assets/side-drawer.js
+++ assets/side-drawer.js
@@ -4,7 +4,35 @@ class SideDrawer extends HTMLElement {
 
     const thisElement = this;
 
+    //Toggle event listeners
+    this.addEventListener('drawer-toggle', function(){
+      if (this.classList.contains('active')) {
+        thisElement.close(this);
+      } else {
+        thisElement.open(this);
+      }
+    })
+
   }
+
+  //Open drawer
+  open(drawer) {
+    this.classList.add('active');
+    // document.body.classList.add('shadow');
+    this.addEventListener('transitionend', () => {
+      drawer.focus();
+    }, { once: true });
+
+    document.body.addEventListener('click', this.onBodyClick, {passive: true});
+  }
+
+  //Close drawer
+  close() {
+    this.classList.remove('active');
+
+    document.body.removeEventListener('click', this.onBodyClick, {passive: true});
+  }
+  
 }
 
 customElements.define('side-drawer', SideDrawer);
